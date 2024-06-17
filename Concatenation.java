8fc03c9e592bf9b084f1656f37e294729f5d484c
Wolfyhub <125683426+Wolfyhub@users.noreply.github.com>
2024-06-17 17:44:32 -0600

    Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,24 @@
+package higherNumberClass;
+
+
+public class ConcatenationClass{
+
+
+public static void main(String[] args) {
+
+
+String str1 = "Hot", str2 = "dog";
+String result = getConcatenate (str1, str2);
+
+System.out.println("Result of String: + result);
+
+}
+
+static String getConcatenate(String str1, String str2){
+String str3 = "";
+str3 = str1 + str2;
+return str3;
+
+
+  }
+}
