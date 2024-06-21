5fce85d271c89c5b1b75e57f5525cb911ad0e901
Wolfyhub <125683426+Wolfyhub@users.noreply.github.com>
2024-06-21 13:27:56 -0600

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+package Calculator;
+
+public class Calculator {
+
+    public int sum(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public int difference(int n1, int n2) {
+        return n1 - n2;
+    }
+}
